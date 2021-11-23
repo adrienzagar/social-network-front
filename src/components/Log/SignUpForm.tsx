2abd7eb30a6371e752f03f1af298194bfb14e046
adrienzagar <adrien.zagar@gmail.com>
2021-11-23 20:41:09 +0100

    signIn/signUp ok

--- src/components/Log/SignUpForm.tsx
+++ src/components/Log/SignUpForm.tsx
@@ -1,16 +1,125 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState,  FormEvent } from "react";
+import axios from "axios";
+import SignInForm from "./SignInForm";
 
 const SignUpForm = () => {
+	const [formSubmit, setFormSubmit] = useState(false);
+	const [pseudo, setPseudo] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [controlPassword, setControlPassword] = useState("");
+	const [pseudoErrorMess, setPseudoError] = useState("");
+	const [emailErrorMess, setEmailError] = useState("");
+	const [passwordErrorMess, setPasswordError] = useState("");
+	const [termsErrorMess, setTermsError] = useState("");
+
+	const handleRegister = async (e :  FormEvent) => {
+		e.preventDefault();
+		const terms = document.getElementById("terms");
+		
+		if (terms !== null ){
+			if (password !== controlPassword || !(terms as HTMLInputElement).checked) {
+				if (password !== controlPassword)
+					
+					setPasswordError("Les mots de passe ne correspondent pas");
+
+				if ( !(terms as HTMLInputElement).checked)
+					setTermsError("Veuillez valider les conditions générales");
+			} else {
+				await axios({
+					method: "post",
+					url: `${process.env.REACT_APP_API_URL}api/user/register`,
+					data: {
+						pseudo,
+						email,
+						password,
+					},
+				})
+					.then((res) => {
+						console.log(res);
+						if (res.data.errors) {
+							setPseudoError(res.data.errors.pseudo);
+							setEmailError(res.data.errors.email);
+							setPasswordError(res.data.errors.password);
+							
+						} else {
+							setFormSubmit(true);
+						}
+					})
+					.catch((err) => console.log(err));
+			}}
+	};
+
 	return (
-		<div>
-            S&apos;inscrire
-		</div>
+		<>
+			{formSubmit ? (
+				<>
+					<SignInForm />
+					<span></span>
+					<h4 className="success">
+            Enregistrement réussi, veuillez-vous connecter
+					</h4>
+				</>
+			) : (
+				<form action="" onSubmit={handleRegister} id="sign-up-form">
+					<label htmlFor="pseudo">Pseudo</label>
+					<br />
+					<input
+						type="text"
+						name="pseudo"
+						id="pseudo"
+						onChange={(e) => setPseudo(e.target.value)}
+						value={pseudo}
+					/>
+					<div className="pseudo error">{pseudoErrorMess}</div>
+					<br />
+					<label htmlFor="email">Email</label>
+					<br />
+					<input
+						type="text"
+						name="email"
+						id="email"
+						onChange={(e) => setEmail(e.target.value)}
+						value={email}
+					/>
+					<div className="email error">{emailErrorMess}</div>
+					<br />
+					<label htmlFor="password">Mot de passe</label>
+					<br />
+					<input
+						type="password"
+						name="password"
+						id="password"
+						onChange={(e) => setPassword(e.target.value)}
+						value={password}
+					/>
+					<div className="password error">{passwordErrorMess}</div>
+					<br />
+					<label htmlFor="password-conf">Confirmer mot de passe</label>
+					<br/>
+					<input
+						type="password"
+						name="password"
+						id="password-conf"
+						onChange={(e) => setControlPassword(e.target.value)}
+						value={controlPassword}
+					/>
+					<div className="password-confirm error">{passwordErrorMess}</div>
+					<br />
+					<input type="checkbox" id="terms" />
+					<label htmlFor="terms">
+            J&apos;accepte les{" "}
+						<a href="/" target="_blank" rel="noopener noreferrer">
+              conditions générales
+						</a>
+					</label>
+					<div className="terms error">{termsErrorMess}</div>
+					<br />
+					<input type="submit" value="Valider inscription" />
+				</form>
+			)}
+		</>
 	);
-};
-
-SignUpForm.propTypes = {
-
 };
 
 export default SignUpForm;
--- src/pages/Profil.tsx
+++ src/pages/Profil.tsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import Log from "../components/Log";
+import { UidContext } from "../components/AppContext";
+// import UpdateProfil from "../components/Profil/UpdateProfil";
 
 const Profil = () : JSX.Element => {
+
+	const uid = useContext(UidContext);
+
 	return (
 		<div className="profil-page">
-			<div className="log-container">
-				<Log signin={false} signup={true} />
-				<div className="img-container">
-					<img src="./img/log.svg" alt="img-log" />
+			{uid ? (
+				<h1>UPDATE PAGE</h1>
+				// <UpdateProfil />
+			) : (
+				<div className="log-container">
+					<Log signin={false} signup={true} />
+					<div className="img-container">
+						<img src="./img/log.svg" alt="img-log" />
+					</div>
 				</div>
-			</div>
+			)}
 		</div>
 	);
 };
